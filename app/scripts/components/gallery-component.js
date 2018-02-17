cc8eaaec019f195f65810bf0d899caa3f39e4877
glauroqj <glauroqj@oi.com.br>
2018-02-17 04:32:40 -0200

    Fix: gitignore

---
+++ app/scripts/components/gallery-component.js
@@ -0,0 +1,20 @@
+(function() {
+
+	Vue.component('gallery', {
+		template: '#gallery',
+		name: 'gallery',
+		props: {
+			images: Array
+		},
+		data() {
+			
+		},
+		mounted() {
+
+		},
+		methods: {
+
+		}
+	});
+
+})();
---
+++ app/scripts/popup.js
@@ -0,0 +1,18 @@
+(function() {
+
+	new Vue({
+		name: 'Get-Image',
+		el: '#getImage',
+		data: {
+			title: 'OLAR'
+		},
+		mounted() {
+			console.log('CARREGOU ')
+		},
+		methods: {
+
+		}
+	});
+
+
+})();

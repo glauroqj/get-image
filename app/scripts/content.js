8536a35e8b9064eb85063e3b7e3e2c267c4ce8ce
glauroqj <glauro.juliani@hotmail.com>
2018-02-17 17:16:17 -0200

    Add: set class on body

--- app/scripts/content.js
+++ app/scripts/content.js
@@ -1,11 +1,43 @@
 (function() {
 
-	chrome.runtime.onMessage.addListener(
-		function(request, sender, sendResponse) {
-			if( request.message === "insert_class_on_body" ) {
-				$('body').addClass('select-image-time');
-			}
+	chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+		if( request.message === "insert_class_on_body" ) {
+			$('body').addClass('select-image-time');
+			watchImageChoice();
 		}
-	);
+	});
+
+
+	function watchImageChoice() {
+		
+		$('body').each(function(index, el) {
+			$('a').removeAttr('href');
+			// console.log( $('a img').attr('src') )
+		});
+
+		$('body').on('click', 'img', function(event) {
+			event.preventDefault();
+			/* Act on the event */
+			let path = event.currentTarget.src
+
+			console.log('ENTRANDO: '+path)
+
+			convertImage(path);
+
+			// window.open(image.src)
+
+
+		});
+
+	}
+
+	function convertImage(path) {
+		let image = new Image();
+		image.src = path;
+
+		let w = window.open('');
+		w.document.write(image.outerHTML);
+
+	}
 
 })();
